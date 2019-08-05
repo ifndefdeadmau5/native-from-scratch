5edb319ed4af99ee954203c98a8c7ac8b97e4a94
박성민 <trevorme91@gmail.com>
2019-08-05 17:11:38 +0900

    Add bottom tab navigator

--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { StyleSheet, Platform, View, Text } from 'react-native';
+import { createMaterialTopTabNavigator, createAppContainer } from 'react-navigation'
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 import { Icon } from 'native-base';
+import CameraTab from './AppTabNavigator/CameraTab';
+import FeedTab from './AppTabNavigator/FeedTab';
 
 export const navigationOptions = {
   headerLeft: <Icon name="ios-camera" style={{ paddingLeft: 10 }} />,
@@ -11,16 +15,23 @@ export const navigationOptions = {
   },
 };
 
-export default () => (
-  <View style={styles.container}>
-    <Text>MainScreen</Text>
-  </View>
-);
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const AppTabNavigator = createMaterialBottomTabNavigator({
+  CameraTab: { screen: CameraTab, navigationOptions: {
+    tabBarIcon: ({ tintColor }) => (
+      <Icon name='camera' style={{ color: tintColor }} />
+  ),
+  } },
+  FeedTab: { screen: FeedTab, navigationOptions: {
+    tabBarIcon: ({ tintColor }) => (
+      <Icon name='ios-home' style={{ color: tintColor }} />
+  ),
+  } },
+}, {
+  initialRouteName: 'CameraTab',
+  activeColor: '#f0edf6',
+  inactiveColor: '#3e2465',
+  barStyle: { backgroundColor: '#694fad' },
+  shifting: true,
 });
+
+export default createAppContainer(AppTabNavigator);
